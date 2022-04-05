ced00ce26e407d2decfa4f72741c101745056c61
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-05 16:03:01 +0800

    完成 goukaou 頁面及路由

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.render('index')
+})
+app.get('/goukaou', (req, res) => {
+  res.render('goukaou')
 })
 
 app.listen(port, () => {
