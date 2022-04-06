278fe07a4dbf7a4e10ab8a32fd69896023700cac
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-06 17:23:54 +0800

    修正 contact 頁面輸入框位置

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const products2 = require('./products2.json')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -8,6 +9,7 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const products = [
   {
@@ -100,6 +102,10 @@ app.get('/offsale-img', (req, res) => {
   res.render('offsale-img', { products: products2[0] })
 })
 app.get('/contact',  (req, res) => {
+  res.render('contact')
+})
+app.post('/contact', (req, res) => {
+  console.log(req.body)
   res.render('contact')
 })
 
