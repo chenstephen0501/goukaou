a30897600a7b51914de4551c2c2b00ba10987e87
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-09 21:51:17 +0800

    重構mongoose

--- models/seeds/productSeeder.js
+++ models/seeds/productSeeder.js
@@ -1,29 +1,11 @@
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const mongoose = require('mongoose')
+
 const productData = require('./product.json')
 const Product = require('../product.js')
-const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-const db = mongoose.connection
-
+const db = require('../../config/mongoose.js')
 db.once('open', () => {
-  console.log('mongodb is connected.')
-  // return
-  //   Product.create({
-  //     name: products[0].name,
-  //     model: products[0].model,
-  //     production: products[0].production,
-  //     introduction: products[0].introduction,
-  //     category: 'mask',
-  //     basePrice: 930,
-  //     sampleImg: products[0].sampleImg,
-  //     image: products[0].imgUrl,
-  //   })
   productData.forEach( async (item, index, arr) => {
     try {
       await Product.create(item)
@@ -35,10 +17,4 @@ db.once('open', () => {
       console.warn(err)
     }
   })
-    // return Product.insertMany(productData)
-    // .then(() => {
-    //   console.log('created is done')
-    //   db.close()
-    // })
-    // .catch((err) => console.error(err))
 })
