a9167ebc96d7f846a092e380d1b58c250079b295
Steavn Chen <tom9876555@yahoo.com.tw>
2022-04-10 02:55:10 +0800

    修改zentai-suit路由圖片編號，資料排序

--- routes/modules/zentai-suit.js
+++ routes/modules/zentai-suit.js
@@ -22,30 +22,81 @@ const linkData = [
 ]
 
 router.get('/', (req, res) => {
-  return Product.find({ category: 'zentai' })
-    .lean()
-    .then((products) => {
-      let imgLength = 0
+  let imgLength = 0
+  return Product.find({ category: 'zentai'})
+  .lean()
+  .then((products) => {
       let newProducts = products.map((item, index) => {
         linkData.forEach((j, jIndex) => {
           if (j.model === item.model) {
-            // if (jIndex === index) {
-            item = { ...item, url: j.url }
-          }
+              item = { ...item, url: j.url }
+            }
         })
         return item
       })
-      newProducts = newProducts.map((item, index) => {
-        item.imgUrl = item.imgUrl.map((j, indexJ) => {
-          j = { ...j, id: imgLength + indexJ + 1 }
-          return j
-        })
-        imgLength += item.imgUrl.length
-        return item
+      newProducts = newProducts.sort((a, b) => {
+        const indexOfa = a.model.indexOf('-')
+        const indexOfb = b.model.indexOf('-')
+        const newA = a.model.substr(indexOfa + 1, 2)
+        const newB = b.model.substr(indexOfb + 1, 2)
+        if (Number(newA) < Number(newB)) {
+          return -1
+        }
       })
+       newProducts = newProducts.map((item, index) => {
+         item.imgUrl = item.imgUrl.map((j, indexJ) => {
+           j = { ...j, id: imgLength + indexJ + 1 }
+           return j
+         })
+         imgLength += item.imgUrl.length
+         return item
+       })
       res.render('zentai-suit', { products: newProducts, imgLength })
     })
     .catch((err) => console.error(err))
 })
 
 module.exports = router
+
+// router.get('/', (req, res) => {
+//   return Product.find({ category: 'zentai' })
+//     .lean()
+//     .then((products) => {
+//       let imgLength = 0
+//       let newProducts = products.map((item, index) => {
+//         linkData.forEach((j, jIndex) => {
+//           if (j.model === item.model) {
+//             // if (jIndex === index) {
+//             item = { ...item, url: j.url }
+//           }
+//         })
+//         return item
+//       })
+//       newProducts = newProducts.map((item, index) => {
+//         item.imgUrl = item.imgUrl.map((j, indexJ) => {
+//           j = { ...j, id: imgLength + indexJ + 1 }
+//           return j
+//         })
+//         imgLength += item.imgUrl.length
+//         return item
+//       })
+//       newProducts = newProducts.sort((a, b) => {
+//         console.log('a.model', a.model)
+//         const indexOfa = a.model.indexOf('-')
+//         const indexOfb = b.model.indexOf('-')
+//         const newA = a.model.substr(indexOfa + 1, 2)
+//         const newB = b.model.substr(indexOfb + 1, 2)
+//         // console.log('newA', newA)
+//         console.log('b.modle', b.model)
+//         // console.log(typeof newA)
+//         console.log(newA, newB)
+//         console.log(Number(newA), Number(newB))
+//         if (Number(newA) < Number(newB)) {
+//           return -1
+//         }
+//       })
+//       console.log(newProducts)
+//       res.render('zentai-suit', { products: newProducts, imgLength })
+//     })
+//     .catch((err) => console.error(err))
+// })
