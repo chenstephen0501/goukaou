06cc476fb5bd6a531377015f45ac99e7726d8f56
Steavn Chen <tom9876555@yahoo.com.tw>
2022-09-19 11:21:11 +0800

    新建 modelSeeder 種子資檔

---
+++ models/seeds/modelSeeder.js
@@ -0,0 +1,20 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const modelData = require('./model.json')
+const Model = require('../model.js')
+const db = require('../../config/mongoose.js')
+db.once('open', () => {
+  modelData.forEach(async (item, index, arr) => {
+    try {
+      await Model.create(item)
+      if (index + 1 === arr.length) {
+        console.log('Model created is done')
+        db.close()
+      }
+    } catch (err) {
+      console.error(errrr)
+    }
+  })
+})
