1755cad4b26de6f933c9f85348a5cc7711c5261e
Steavn Chen <tom9876555@yahoo.com.tw>
2022-09-19 11:04:50 +0800

    修改 productSeeder 種子資料

--- models/seeds/productSeeder.js
+++ models/seeds/productSeeder.js
@@ -5,14 +5,17 @@ if (process.env.NODE_ENV !== 'production') {
 const productData = require('./product.json')
 const Product = require('../product.js')
 const Category = require('../category.js')
+const Model = require('../model.js')
 const db = require('../../config/mongoose.js')
 db.once('open', async () => {
   try {
     const categoryData = await Category.find()
+    const modelData = await Model.find()
     productData.forEach( async (item, index, arr) => {
       try {
         const categoryId = categoryData.find(item2 => item2.name === item.category).id
-        await Product.create({ ...item, categoryId })
+        const modelId = modelData.find(item2 => item2.name === item.model).id
+        await Product.create({ ...item, categoryId, modelId })
         if (index + 1 === arr.length) {
           console.log('Product created is done')
           db.close()
