c03131b8a29427b14a1f2bef06b051956ec9ce6c
Steavn Chen <tom9876555@yahoo.com.tw>
2022-09-19 12:35:58 +0800

    創建 POST admin/models 路由

--- routes/modules/models.js
+++ routes/modules/models.js
@@ -12,4 +12,23 @@ router.get('/', (req, res) => {
     })
 })
 
+router.post('/', (req, res) => {
+  const { name } = req.body
+  if (!name) { 
+    return res.redirect('back')
+  }
+  return Promise.all([
+    Model.find().lean(),
+    Model.findOne({ name }).lean()
+  ]) 
+    .then(([models, model]) => {
+      if (!model) {
+        return Model.create({ name })
+          .then(() => res.redirect('back'))
+      }
+      return res.render('admin/model', { models, model })
+    })
+    .catch(err => console.error(err))
+})
+
 module.exports = router
