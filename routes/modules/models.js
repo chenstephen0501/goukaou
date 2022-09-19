24725ffe101f7ad16dc3b3602afa1fcfb55cc777
Steavn Chen <tom9876555@yahoo.com.tw>
2022-09-19 13:07:05 +0800

    新建 GET admin/model/modelId 路由

--- routes/modules/models.js
+++ routes/modules/models.js
@@ -31,4 +31,41 @@ router.post('/', (req, res) => {
     .catch(err => console.error(err))
 })
 
+router.get('/:modelId', (req, res) => {
+  const modelId = req.params.modelId
+  return Promise.all([
+    Model.find().lean(),
+    Model.findById({ _id: modelId }).lean()
+  ])
+    .then(([models, model]) => {
+      return res.render('admin/model' ,{models, model })
+    })
+})
+
+router.put('/:modelId', (req, res) => {
+  const modelId = req.params.modelId
+  const { name } = req.body
+  if (!name) {
+    return res.redirect('back')
+  }
+  return Promise.all([
+    Model.find({ _id: { $nin: [modelId] } }).lean(),
+    Model.findById().lean()
+  ])
+    .then(([models, model]) => {
+      const checkModel = models.some(item => item.name === name)
+      if (checkModel) {
+        return res.redirect('back')
+      }
+      return Model.create({ _id: modelId }, { name })
+      // if (!model) {
+      //   return Model.create({ name })
+      //     .then(() => res.redirect('back'))
+      // }
+      // return res.render('admin/model', { models, model })
+    })
+    .then(() => res.redirect('/admin/model'))
+    .catch(err => console.error(err))
+})
+
 module.exports = router
