cdc64de576facee406358a8fe950fc8120371cb3
Steavn Chen <tom9876555@yahoo.com.tw>
2022-09-23 13:14:16 +0800

    在 middleware 新增 multer.js 檔 並把套件設定遷移到此檔

--- routes/modules/products.js
+++ routes/modules/products.js
@@ -1,8 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const multer = require('multer')
-const upload = multer({ dest: 'temp/' })
+const upload = require('../../middleware/multer.js')
 
 const fs = require('fs')
 const imgur = require('imgur')
