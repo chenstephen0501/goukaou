37fde25d5a5d0a4c56f9187dcd8662e8c8a39fa4
Steavn Chen <tom9876555@yahoo.com.tw>
2022-10-02 00:54:42 +0800

    新增 admin postProduct api

--- controllers/apis/admin-controller.js
+++ controllers/apis/admin-controller.js
@@ -32,73 +32,7 @@ const adminController = {
     adminServices.createProduct(req, (err, data) => err ? next(err) : res.json(data))
   },
   postProduct: (req, res, next) => {
-    const { name, categoryId, basePrice, highestPrice, production, introduction, modelId } = req.body
-    const { sampleImg, imgUrl } = req.files
-    if (!name || !categoryId || !basePrice, !highestPrice || !production || !modelId) throw new Error('新增的產品所有欄位都要填寫。')
-    if (sampleImg || imgUrl) {
-      return Promise.all([
-        Product.findOne({ name }).lean(),
-        imgurFileHandler(sampleImg),
-        imgurManyFileHandler(imgUrl),
-        Category.find(),
-        Model.find()
-      ]).then(([checkName, productSampleImg, productImgUrl, categories, models]) => {
-        if (checkName) throw new Error('這個名稱己用過，請更換!')
-        if (!categories) throw new Error('找不到類別資料!')
-        if (!models) throw new Error('找不到模型資料!')
-        const category = categories.find(item => item._id.toString() === categoryId.toString()).name
-        const model = models.find(item => item._id.toString() === modelId.toString()).name
-        return Product.create({
-          name,
-          category,
-          model,
-          basePrice,
-          highestPrice,
-          production,
-          introduction,
-          sampleImg: sampleImg ? productSampleImg : null,
-          imgUrl: imgUrl ? productImgUrl : null,
-          categoryId,
-          modelId
-        })
-          .then(product => {
-            req.flash('success_messages', '成功新增產品。')
-            return res.redirect('/admin/products')
-          })
-      })
-        .catch(err => next(err))
-    } else {
-      return Promise.all([
-        Product.findOne({ name }).lean(),
-        Category.find(),
-        Model.find()
-      ])
-        .then(([checkName, categories, models]) => {
-          if (checkName) throw new Error('這個名稱己用過，請更換!')
-          if (!categories) throw new Error('找不到類別資料!')
-          if (!models) throw new Error('找不到模型資料!')
-          const category = categories.find(item => item._id.toString() === categoryId.toString()).name
-          const model = models.find(item => item._id.toString() === modelId.toString()).name
-          return Product.create({
-            name,
-            category,
-            model,
-            basePrice,
-            highestPrice,
-            production,
-            introduction,
-            sampleImg: null,
-            imgUrl: null,
-            categoryId,
-            modelId
-          })
-        })
-        .then(product => {
-          req.flash('success_messages', '成功新增產品。')
-          return res.redirect('/admin/products')
-        })
-        .catch(err => next(err))
-    }
+    adminServices.postProduct(req, (err, data) => err ? next(err) : res.json(data))
   },
   getProduct: (req, res, next) => {
     const productId = req.params.productId
--- services/admin-services.js
+++ services/admin-services.js
@@ -42,6 +42,77 @@ const adminController = {
       })
       .catch(err => cb(err))
   },
+  postProduct: (req, cb) => {
+    const { name, categoryId, basePrice, highestPrice, production, introduction, modelId } = req.body
+    const { sampleImg, imgUrl } = req.files
+    if (!name || !categoryId || !basePrice, !highestPrice || !production || !modelId) throw new Error('新增的產品所有欄位都要填寫。')
+    if (sampleImg || imgUrl) {
+      return Promise.all([
+        Product.findOne({ name }).lean(),
+        imgurFileHandler(sampleImg),
+        imgurManyFileHandler(imgUrl),
+        Category.find(),
+        Model.find()
+      ]).then(([checkName, productSampleImg, productImgUrl, categories, models]) => {
+        if (checkName) throw new Error('這個名稱己用過，請更換!')
+        if (!categories) throw new Error('找不到類別資料!')
+        if (!models) throw new Error('找不到模型資料!')
+        const category = categories.find(item => item._id.toString() === categoryId.toString()).name
+        const model = models.find(item => item._id.toString() === modelId.toString()).name
+        return Product.create({
+          name,
+          category,
+          model,
+          basePrice,
+          highestPrice,
+          production,
+          introduction,
+          sampleImg: sampleImg ? productSampleImg : null,
+          imgUrl: imgUrl ? productImgUrl : null,
+          categoryId,
+          modelId
+        })
+          .then(product => {
+            // req.flash('success_messages', '成功新增產品。')
+            const data = product
+            return cb(null, { product: data })
+            // return res.redirect('/admin/products')
+          })
+      })
+        .catch(err => cb(err))
+    } else {
+      return Promise.all([
+        Product.findOne({ name }).lean(),
+        Category.find(),
+        Model.find()
+      ])
+        .then(([checkName, categories, models]) => {
+          if (checkName) throw new Error('這個名稱己用過，請更換!')
+          if (!categories) throw new Error('找不到類別資料!')
+          if (!models) throw new Error('找不到模型資料!')
+          const category = categories.find(item => item._id.toString() === categoryId.toString()).name
+          const model = models.find(item => item._id.toString() === modelId.toString()).name
+          return Product.create({
+            name,
+            category,
+            model,
+            basePrice,
+            highestPrice,
+            production,
+            introduction,
+            sampleImg: null,
+            imgUrl: null,
+            categoryId,
+            modelId
+          })
+        })
+        .then(product => {
+          const data = product
+          return cb(null, { product: data })
+        })
+        .catch(err => cb(err))
+    }
+  },
 }
 
 module.exports = adminController
