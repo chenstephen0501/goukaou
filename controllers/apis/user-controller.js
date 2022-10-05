3b051707836a275144ba827c2ee884af72ca8851
Steavn Chen <tom9876555@yahoo.com.tw>
2022-10-05 21:19:57 +0800

    將controll/apis/user-controllre.js 裡的 login, register 抽取至 user-services.js

--- controllers/apis/user-controller.js
+++ controllers/apis/user-controller.js
@@ -1,74 +1,12 @@
-const jwt = require('jsonwebtoken')
-
-const bcrypt = require('bcryptjs')
-const User = require('../../models/user.js')
+const userServices = require('../../services/user-services.js')
 
 const userController = {
   login: (req, res, next) => {
-    const { email, password } = req.body
-    if (!email || !password) {
-      const err = new Error('密碼或電子郵件欄位錯誤')
-      err.status = 401
-      throw err
-    }
-    return User.findOne({ email })
-      .lean()
-      .then(user => {
-        if (!user) {
-          const err = new Error('密碼或電子郵件錯誤')
-          err.status = 401
-          throw err
-        }
-        return bcrypt.compare(password, user.password)
-          .then(isMatch => {
-            if (!isMatch) {
-              const err = new Error('密碼或電子郵件錯誤!')
-              err.status = 401
-              throw err
-            }
-            try {
-              delete user.password
-              const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '30d' })
-              res.json({
-                'status': 'success', data: {
-                  token,
-                  user
-                }
-              })
-            } catch (err) {
-              next(err)
-            }
-          })
-      })
-      .catch(err => next(err))
+    userServices.login(req, (err, data) => err ? next(err) : res.json({ 'status': 'success', data }))
   },
   register: (req, res, next) => {
-    const { name, email, password, passwordCheck } = req.body
-    if (!name || !email || !password || !passwordCheck) throw new Error('所有欄位都要填寫。')
-    if (password !== passwordCheck) throw new Error('密碼與確認密不相同。')
-    return Promise.all([
-      User.find({ name }).lean(),
-      User.find({ email }).lean(),
-    ])
-      .then(([checkName, checkEmail]) => {
-        if (checkEmail[0]) throw new Error('此郵件己被註冊。')
-        if (checkName[0]) throw new Error('此名字己被使用。')
-        return bcrypt.genSalt(10)
-      })
-      .then(salt => bcrypt.hash(password, salt))
-      .then(hash => {
-        return User.create({
-          name,
-          email,
-          password: hash
-        })
-      })
-      .then((user) => {
-        return res.json({ 'status': 'success', message: '註冊成功' })
-      })
-      .catch(err => next(err))
+    userServices.register(req, (err, data) => err ? next(err) : res.json({ 'status': 'success', data}))
   }
-
 }
 
 module.exports = userController
--- services/user-services.js
+++ services/user-services.js
@@ -1,14 +1,84 @@
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
-const passport = require('passport')
 
 const User = require('../models/user.js')
 
 const userServices = {
-  loginPage: (req, cb) => {
-    return cb(null, {})
+  login: (req, cb) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+      const err = new Error('密碼或電子郵件欄位錯誤')
+      err.status = 401
+      throw err
+    }
+    return User.findOne({ email })
+      .lean()
+      .then(user => {
+        if (!user) {
+          const err = new Error('密碼或電子郵件錯誤')
+          err.status = 401
+          throw err
+        }
+        return bcrypt.compare(password, user.password)
+          .then(isMatch => {
+            if (!isMatch) {
+              const err = new Error('密碼或電子郵件錯誤!')
+              err.status = 401
+              throw err
+            }
+            try {
+              delete user.password
+              const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '30d' })
+              cb(null, { token, user})
+            } catch (err) {
+              next(err)
+            }
+          })
+      })
+      .catch(err => cb(err))
   },
-  registerPage: (req, cb) => {
-    return cb(null, {})
+  register: (req, cb) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (!name || !email || !password || !passwordCheck) {
+      const err = new Error('所有欄位都要填寫。')
+      err.status = 401
+      throw err
+    }
+    if (password !== passwordCheck) {
+      const err = new Error('密碼與確認密不相同。')
+      err.status = 401
+      throw err
+    }
+    return Promise.all([
+      User.find({ name }).lean(),
+      User.find({ email }).lean(),
+    ])
+      .then(([checkName, checkEmail]) => {
+        if (checkEmail[0]) {
+          const err = new Error('此郵件己被註冊。')
+          err.status = 401
+          throw err
+        }
+        if (checkName[0]) {
+          const err = new Error('此名字己被註冊。')
+          err.status = 401
+          throw err
+        }
+        return bcrypt.genSalt(10)
+      })
+      .then(salt => bcrypt.hash(password, salt))
+      .then(hash => {
+        return User.create({
+          name,
+          email,
+          password: hash
+        })
+      })
+      .then((user) => {
+        const { password, ...result } = user.toJSON()
+        cb(null, { user: result })
+      })
+      .catch(err => cb(err))
   }
 }
 
